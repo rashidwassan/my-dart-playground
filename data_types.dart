a0e625929dfd50d9451f3444f5b3a8578ab2b137
Rashid Wassan <rashidwassaan@gmail.com>
2021-11-11 17:12:00 +0500

    added num

---
+++ data_types.dart
@@ -0,0 +1,8 @@
+// the code example in this file shows different built-in Dart data types
+void main() {
+  // Numbers
+  // when it comes to numbers dart has 'num' data type, a parent of int & double
+  num number = 1;
+  // num can be assigned int or double value.
+  number = 2.0;
+}
